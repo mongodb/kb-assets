63a74e8b9c3400dc0702ef8321d986e85834a373
iammfh <155508077+iammfh@users.noreply.github.com>
2025-10-20 07:21:24 -0700

    add mongo-ftdc

---
+++ mongo-ftdc/build.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# Copyright 2019-present Kuei-chun Chen. All rights reserved.
+die() { echo "$*" 1>&2 ; exit 1; }
+[[ "$(which go)" = "" ]] && die "go command not found"
+EXEC=ftdc_json
+VERSION="v$(cat version)-$(git log -1 --date=format:"%Y%m%d" --format="%ad")"
+LDFLAGS="-X main.version=$VERSION -X main.repo=$EXEC"
+
+mkdir -p dist
+if [ "$1" == "docker" ]; then
+	VER=$(cat version)
+	TAG="simagix/ftdc"
+    docker-compose down > /dev/null 2>&1
+    if [[ "${VER}" == "master" ]]; then
+        VER="latest"
+    fi
+    docker build --no-cache -f Dockerfile -t ${TAG}:${VER} .
+	docker tag ${TAG}:${VER} ${TAG}
+
+    docker build --no-cache -f grafana/Dockerfile -t simagix/grafana-ftdc:${VER} .
+    docker tag simagix/grafana-ftdc:${VER} simagix/grafana-ftdc
+else
+    env CGO_ENABLED=0 go build -ldflags "$LDFLAGS" -o dist/$EXEC main/ftdc_json.go
+    dist/$EXEC -version
+fi
